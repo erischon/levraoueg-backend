1abd853bc66df88e539958aa72c59c9b4aab6337
Eri Schon <erischon@gmail.com>
2022-06-19 18:05:49 +0200

    Update propertyRoutes.js

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -1,38 +1,29 @@
 const express = require("express");
 
 const Property = require("../models/propertyModel");
-const { getProperties } = require("../controllers/propertyController");
+const {
+  getAllProperty,
+  getProperty,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+} = require("../controllers/propertyController");
 
 const router = express.Router();
 
-// GET all properties
-router.get("/", getProperties);
+// GET all property
+router.get("/", getAllProperty);
 
 // GET a single property
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET a single property" });
-});
+router.get("/:id", getProperty);
 
 // POST a new property
-router.post("/", async (req, res) => {
-  const { title, propertyType, notes } = req.body;
-
-  try {
-    const property = await Property.create({ title, propertyType, notes });
-    res.status(200).json(property);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
-
-// DELETE a property
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "DELETE a property" });
-});
+router.post("/", createProperty);
 
 // UPDATE a property
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "UPDATE a property" });
-});
+router.patch("/:id", updateProperty);
+
+// DELETE a property
+router.delete("/:id", deleteProperty);
 
 module.exports = router;
