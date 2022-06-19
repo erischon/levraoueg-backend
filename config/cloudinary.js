a70df34c253753caec2154efea25648f27a6e664
Eri Schon <erischon@gmail.com>
2022-06-19 12:01:12 +0200

    Create cloudinary.js

---
+++ config/cloudinary.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+import cloudinary from "cloudinary";
+
+// Setting up cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
