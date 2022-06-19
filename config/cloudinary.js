565ed350eb6476f8cbf59611848ae1a436902e35
Eri Schon <erischon@gmail.com>
2022-06-19 18:05:37 +0200

    Delete cloudinary.js

--- config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-import cloudinary from "cloudinary";
-
-// Setting up cloudinary config
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
